940e102c653a33123db8e8e193c8dab1fd3af756
9leincr <redpostoffice@gmail.com>
2021-08-26 11:50:00 +0900

    Update hosts 2021-08

--- convert2hosts/controller.py
+++ convert2hosts/controller.py
@@ -11,7 +11,7 @@ class Controller:
     def setData(self, url, style):
         """Get the data & Set it"""
         url = self.translate(url)
-        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0)'
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
         headers = {'User-Agent': user_agent}
         req = urllib.request.Request(url, headers=headers)
         try:
